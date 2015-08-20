9004e2c44779261ba1bfddfe585f672a53dd0af7
Azurebeats <azurebeats@hotmail.nl>
2015-08-20 11:21:51 +0200

    Error Handling Galore
    Made sure that the updater has far less opportunities to crash on its
    own, it will display a fairly generic issue highlighting what might have
    gone wrong.

--- Yet Another Game Patcher/MainWindow.xaml.cs
+++ Yet Another Game Patcher/MainWindow.xaml.cs
@@ -33,23 +33,26 @@ namespace Yet_Another_Game_Patcher {
         private void Window_Initialized(Object sender, EventArgs e) {
             var settings = AppDomain.CurrentDomain.BaseDirectory + "\\Patcher.xml";
             if (File.Exists(settings)) {
-                foreach (var el in XElement.Load(File.OpenRead(settings)).Elements()) {
-                    Options.Add(el.Attribute("key").Value, el.Attribute("value").Value);
-                }
-                this.Title = Options["GameName"];
-                Browser.Navigate(Options["NewsPage"]);
+                try {
+                    foreach (var el in XElement.Load(File.OpenRead(settings)).Elements()) {
+                        Options.Add(el.Attribute("key").Value, el.Attribute("value").Value);
+                    }
+                    this.Title = Options["GameName"];
+                    Browser.Navigate(Options["NewsPage"]);
+                } catch { ReportError("An error has occured while attempting to apply the settings."); return; }
             } else {
-                MessageBox.Show("Unable to locate 'Patcher.xml'!", "File not Found", MessageBoxButton.OK, MessageBoxImage.Error);
-                this.Close();
+                ReportError("Unable to locate 'Patcher.xml'!"); 
                 return;
             }
 
-            var data        = new WebClient().DownloadData(Options["RemoteDirectory"] + "Checksums.xml");
-            var remotefiles = XElement.Load(new MemoryStream(data));
-            var worker      = new BackgroundWorker();
-            worker.DoWork             += new DoWorkEventHandler(CompareFiles);
-            worker.RunWorkerCompleted += (s, a) => { PlayButton.Dispatcher.BeginInvoke((Action)(() => { ContentLabel.Content = "Ready."; PlayButton.IsEnabled = true; })); };
-            worker.RunWorkerAsync(remotefiles);
+            try {
+                var data = new WebClient().DownloadData(Options["RemoteDirectory"] + "Checksums.xml");
+                var remotefiles = XElement.Load(new MemoryStream(data));
+                var worker = new BackgroundWorker();
+                worker.DoWork += new DoWorkEventHandler(CompareFiles);
+                worker.RunWorkerCompleted += (s, a) => { PlayButton.Dispatcher.BeginInvoke((Action)(() => { ContentLabel.Content = "Ready."; PlayButton.IsEnabled = true; })); };
+                worker.RunWorkerAsync(remotefiles);
+            } catch { ReportError("An error has occured while processing the remote data."); return; }
         }
 
         private void CompareFiles(Object sender, DoWorkEventArgs e) {
@@ -57,38 +60,50 @@ namespace Yet_Another_Game_Patcher {
             var total   = files.Elements().Count();
             var current = 0;
             this.ThreadSafe(() => { TotalProgress.Maximum = total; });
-            foreach (var file in files.Elements()) {
-                var dir         = AppDomain.CurrentDomain.BaseDirectory + "\\";
-                var name        = file.Attribute("name").Value;
-                var rhash       = file.Attribute("hash").Value;
-                var download    = false;
-                this.ThreadSafe(() => { ContentLabel.Content = String.Format("Checking {0}...", name); });
-                if (!File.Exists(dir + name)) {
-                    download = true;
-                } else {
-                    using (var f = File.OpenRead(dir + name)) {
-                        var lhash = f.ToSha256();
-                        if (!String.Equals(lhash, rhash)) download = true;
+            try {
+                foreach (var file in files.Elements()) {
+                    var dir = AppDomain.CurrentDomain.BaseDirectory + "\\";
+                    var name = file.Attribute("name").Value;
+                    var rhash = file.Attribute("hash").Value;
+                    var download = false;
+                    this.ThreadSafe(() => { ContentLabel.Content = String.Format("Checking {0}...", name); });
+                    if (!File.Exists(dir + name)) {
+                        download = true;
+                    } else {
+                        using (var f = File.OpenRead(dir + name)) {
+                            var lhash = f.ToSha256();
+                            if (!String.Equals(lhash, rhash)) download = true;
+                        }
                     }
-                }
 
-                if (download) {
-                    this.ThreadSafe(() => { ContentLabel.Content = String.Format("Downloading {0}...", name); });
-                    if (!Directory.Exists(System.IO.Path.GetDirectoryName(dir + name))) Directory.CreateDirectory(System.IO.Path.GetDirectoryName(dir + name));
-                    if (File.Exists(dir + name)) File.Delete(dir + name);
-                    using (var client = new WebClient()) {
-                        client.DownloadFile(Options["RemoteDirectory"] + name, dir + name);
+                    if (download) {
+                        this.ThreadSafe(() => { ContentLabel.Content = String.Format("Downloading {0}...", name); });
+                        if (!Directory.Exists(System.IO.Path.GetDirectoryName(dir + name))) Directory.CreateDirectory(System.IO.Path.GetDirectoryName(dir + name));
+                        if (File.Exists(dir + name)) File.Delete(dir + name);
+                        using (var client = new WebClient()) {
+                            try {
+                                client.DownloadFile(Options["RemoteDirectory"] + name, dir + name);
+                            } catch { ReportError(String.Format("An error has occured while attempting to download '{0}'", name)); return; }
+                        }
                     }
-                }
 
-                current++;
-               this.ThreadSafe(() => { TotalProgress.Value = current; });
-            }
+                    current++;
+                    this.ThreadSafe(() => { TotalProgress.Value = current; });
+                }
+            } catch { ReportError("A fatal error has occured while attempting to process the updates."); return; }
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e) {
             var dir = AppDomain.CurrentDomain.BaseDirectory + "\\";
-            Process.Start(dir + Options["GameClient"]);
+            try {
+                Process.Start(dir + Options["GameClient"]);
+            } catch { ReportError(String.Format("Unable to locate '{0}'!", Options["GameClient"])); return; }
         }
+
+        private void ReportError(String msg) {
+            MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            this.Close();
+        }
+
     }
 }
